ad56fcf19e99397fbb41c4791a004749da21af03
Tim Ebbinghaus <tim.ebbinghaus@eshaker.de>
2023-10-06 14:56:39 +0200

    task: render svg icons to png

--- go.mod
+++ go.mod
@@ -4,22 +4,40 @@ go 1.20
 
 require (
 	fyne.io/fyne/v2 v2.3.5
+	fyne.io/systray v1.10.1-0.20230602210930-b6a2d6ca2a7b
+	github.com/davecgh/go-spew v1.1.1
 	github.com/muesli/gamut v0.3.1
 	github.com/rs/zerolog v1.29.1
+	github.com/tdewolff/canvas v0.0.0-20230929121632-db7265801182
 )
 
 require (
-	github.com/fredbi/uri v0.1.0 // indirect
+	github.com/ByteArena/poly2tri-go v0.0.0-20170716161910-d102ad91854f // indirect
+	github.com/benoitkugler/textlayout v0.3.0 // indirect
+	github.com/benoitkugler/textprocessing v0.0.3 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/fredbi/uri v1.0.0 // indirect
+	github.com/go-fonts/latin-modern v0.3.0 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-text/typesetting v0.0.0-20230502123426-87572f5551cf // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/muesli/clusters v0.0.0-20200529215643-2700303c1762 // indirect
 	github.com/muesli/kmeans v0.3.1 // indirect
-	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
-	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/tdewolff/minify/v2 v2.12.4 // indirect
+	github.com/tdewolff/parse/v2 v2.6.5 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	github.com/wcharczuk/go-chart/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/image v0.3.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	gonum.org/v1/plot v0.12.0 // indirect
+	star-tex.org/x/tex v0.4.0 // indirect
 )
--- icons.go
+++ icons.go
@@ -1,12 +1,15 @@
 package tabler_icons
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
 	"github.com/muesli/gamut"
 	"github.com/rs/zerolog"
+	"github.com/tdewolff/canvas"
+	"github.com/tdewolff/canvas/renderers"
 	"image/color"
 	"io"
 	"sync"
@@ -29,6 +32,25 @@ func cacheIdentifier(name string, stainColor color.Color) string {
 	return fmt.Sprintf("%s(%s)", name, gamut.ToHex(stainColor))
 }
 
+func Source(name string) ([]byte, error) {
+
+	if f, err := resources.Open("icons/" + name + ".svg"); err != nil {
+		return nil, err
+	} else if icon, readErr := io.ReadAll(f); readErr != nil {
+		return nil, readErr
+	} else {
+		return icon, nil
+	}
+}
+
+func StainedSource(name string, stainColor color.Color) ([]byte, error) {
+	if source, err := Source(name); err != nil {
+		return nil, err
+	} else {
+		return bytes.Replace(source, []byte("currentColor"), []byte(gamut.ToHex(stainColor)), -1), nil
+	}
+}
+
 func Icon(name string, stainColor ...color.Color) (fyne.Resource, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -41,10 +63,8 @@ func Icon(name string, stainColor ...color.Color) (fyne.Resource, error) {
 	if i, exists := cache[cacheId]; exists {
 		return i, nil
 	}
-	if f, err := resources.Open("icons/" + name + ".svg"); err != nil {
+	if icon, err := Source(name); err != nil {
 		return nil, err
-	} else if icon, readErr := io.ReadAll(f); readErr != nil {
-		return nil, readErr
 	} else {
 		// tabler icons must be stained before use
 		cache[cacheId] = NewStainedSVGResource(name, icon, stainColor[0])
@@ -54,6 +74,32 @@ func Icon(name string, stainColor ...color.Color) (fyne.Resource, error) {
 		return cache[cacheId], nil
 	}
 
+}
+
+func PNG(name string, stainColor ...color.Color) ([]byte, error) {
+	var (
+		source []byte
+		err    error
+	)
+	if len(stainColor) > 0 {
+		source, err = StainedSource(name, stainColor[0])
+	} else {
+		source, err = Source(name)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if svg, parseErr := canvas.ParseSVG(bytes.NewBuffer(source)); parseErr != nil {
+		return nil, parseErr
+	} else {
+		var pngBuffer bytes.Buffer
+		if rasterErr := renderers.PNG()(&pngBuffer, svg); rasterErr != nil {
+			return nil, rasterErr
+		} else {
+			return pngBuffer.Bytes(), nil
+		}
+	}
 }
 
 func MustIcon(name string, stainColor ...color.Color) fyne.Resource {
